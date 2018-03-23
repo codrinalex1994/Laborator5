3c05198e447b09502ebc55a4e5fa638992e56f4e
Petru <pitimihai@gmail.com>
2018-03-23 08:43:56 +0200

    sg

--- src/Admin.java
+++ src/Admin.java
@@ -1,13 +1,34 @@
 import java.util.Vector;
+import java.util.List;
+import java.util.ArrayList;
 
 public class Admin {
 
-    public Vector  myCommand;
+    public List<Command>  myCommand = new ArrayList<>();
 
-  public void getActiveCommands() {
+  public List<Command> getActiveCommands() {
+    List<Command> list = new ArrayList<>();
+
+    // 0 - inactiv, 1 - in pregatire, 2 - pe drum, 3 - finished
+    for(Command command : this.myCommand) {
+        if(command.getStatus() == 1 || command.getStatus() == 2) {
+            list.add(command);
+        }
+    }
+
+    return list;
   }
 
-  public void getFinishedCommands() {
+  public List<Command> getFinishedCommands() {
+      List<Command> list = new ArrayList<>();
+
+      for(Command command : this.myCommand) {
+          if(command.getStatus() == 0) {
+              list.add(command);
+          }
+      }
+
+      return list;
   }
 
 }
