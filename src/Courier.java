7c3c758b1c6aba33af3dc3f8da382f3d1c763dad
codrinalex1994 <codrinalex1994@yahoo.com>
2018-03-23 09:01:45 +0200

    curier

--- src/Courier.java
+++ src/Courier.java
@@ -1,13 +1,10 @@
 import java.util.ArrayList;
-import java.util.Vector;
 
 public class Courier {
 
   private ArrayList<String> locatii = new ArrayList<>();
 
   private String location;
-
-  private Vector  myCommand;
 
   public String getLocation() {
     return location;
@@ -16,14 +13,6 @@ public class Courier {
   public void updateLocation(String location) {
     addLocatie(this.location);
     this.location = location;
-  }
-
-  public Vector getMyCommand() {
-    return myCommand;
-  }
-
-  public void setMyCommand(Vector myCommand) {
-    this.myCommand = myCommand;
   }
 
   public ArrayList<String> getCommands() {
