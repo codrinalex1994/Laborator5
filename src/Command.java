6e1fbc114e8018111f50921922fe910967eb9e46
Narcis <narcis_diaconu_98@yahoo.com>
2018-03-23 08:54:36 +0200

    Command type

--- src/Command.java
+++ src/Command.java
@@ -2,6 +2,8 @@ public class Command {
 
     private static int count=0;
     private int id;
+
+    // 0 - inactiv, 1 - in pregatire, 2 - pe drum, 3 - finished
     private int status;
     private Courier courier = new Courier();
 
@@ -10,8 +12,19 @@ public class Command {
         this.status = 0;
     }
 
-    public int getStatus() {
-        return this.status;
+    public String getStatus() {
+        switch(status){
+            case 0:
+                return "inactive";
+            case 1:
+                return "in progress";
+            case 2:
+                return "in delivery";
+            case 3:
+                return "delivered";
+            default:
+                return "unknown";
+        }
     }
 
     public int getId() {
